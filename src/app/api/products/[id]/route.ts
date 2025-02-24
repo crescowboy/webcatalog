21d5bae03dfbb660a85a86d7dbbff07ae2355b4e
wcrespo <wellingtoncrespo2001@gmail.com>
2025-02-23 22:51:35 -0500

    conection db

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/mongodb";
+import Product from "@/models/Product";
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  try {
+    await connectDB();
+    const { id } = params;
+
+    if (!id) {
+      return NextResponse.json({ message: "ID del producto requerido" }, { status: 400 });
+    }
+
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return NextResponse.json({ message: "Producto no encontrado" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Producto eliminado exitosamente" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error al eliminar el producto", error }, { status: 500 });
+  }
+}
